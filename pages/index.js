e125827ab1287152390e8f9859ac9ca5ddb80bdb
Doug McDonald <dougajmcdonald@gmail.com>
2019-03-31 22:03:32 +0100

    add dynamic link maker

--- pages/index.js
+++ pages/index.js
@@ -2,21 +2,17 @@ import React from 'react'
 import Layout from '../components/core/Layout'
 import BoxNav from '../components/navigation/BoxNav'
 
-const links = [
-  { text: 'Home', href: '/index' },
-  { text: 'Component 2', href: '/about' },
-  { text: 'Component 3', href: '/page3' },
-  { text: 'Component 4', href: '/page4' },
-  { text: 'Component 5', href: '/page5' },
-  { text: 'Component 6', href: '/page6' },
-  { text: 'Component 7', href: '/page7' },
-  { text: 'Component 8', href: '/page8' },
-  { text: 'Component 9', href: '/page9' }
-]
+const makeLinks = number => {
+  const links = []
+  for (let i = 0; i < number; i++) {
+    links.push({ text: `Link ${i}`, href: `/link${i}` })
+  }
+  return links
+}
 
 const Index = () => (
   <Layout>
-    <BoxNav links={links} />
+    <BoxNav links={makeLinks(18)} />
   </Layout>
 )
 
